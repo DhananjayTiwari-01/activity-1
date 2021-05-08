cad49174e8e3e5d0c167c9c3e93479d74db54a1a
Dhananjay Tiwari <83721566+DhananjayTiwari-01@users.noreply.github.com>
2021-05-08 10:41:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,64 +1,30 @@
-
+var fixedRect, movingRect;
 
 function setup() {
-  createCanvas(800,400);
-  
-  fixedRect = createSprite(200, 200, 50, 80);
-  fixedRect.shapeColor = "green" 
-   movingRect = createSprite(400,200,80,80) 
-   movingRect.shapeColor = "green"
-   
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
 
-   object1 = createSprite(200, 50, 50, 80);
-   object2 = createSprite(100, 80, 50, 80);
-   object3 = createSprite(300, 100, 50, 80);
-   object4 = createSprite(40, 400, 50, 80);
-   
-   object1.shapeColor = "green"
-   object2.shapeColor = "green"
-   object3.shapeColor = "green"
-   object4.shapeColor = "green"
-
-   
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
-function draw(){
-  
-  background(255,255,255); 
-  if(isTouching()){
+function draw() {
+  background(0,0,0);  
 
-  fixedRect.shapeColor = "red" 
-  movingRect.shapeColor = "red" 
-
-
-
-}
-
-else{
-  fixedRect.shapeColor = "green" 
-  movingRect.shapeColor = "green" 
-
-}
-
-      drawSprites(); 
-    
-    }
-
-
-function isTouching(){
-
-  
- if(object1.x - object2.x < object2.width/2 + object1.width/2 && object2.x - object1.x < object2.width/2 + object1.width/2 && object1.y - object2.y < object2.height/2 + object1.height/2 && object2.y - object1.y < object2.height/2 + object1.height/2){     
-
-    return true
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
   }
-
-  else{
-
-    return false
-
+  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
   }
-
- 
-
+  drawSprites();
 }
